a7d6e34c5de46cc2b8356886525d2c233d884ae6
Raul Pele <raul.pele2001@gmail.com>
2022-08-09 12:57:05 +0300

    Initial Commit

---
+++ Move/MoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveApp.swift
+//  Move
+//
+//  Created by Raul Pele on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
