4ea4267a779f91f6792633a94eb4b718e6ea89aa
Raul Pele <raul.pele2001@gmail.com>
2022-08-10 12:29:20 +0300

    added view for the splash screen

--- Move/Views/ContentView.swift
+++ Move/Views/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("hello") {}
-            .background(Color.primaryDark)
+        SplashView()
     }
 }
 
---
+++ Move/Views/SplashViews/SplashView.swift
@@ -0,0 +1,27 @@
+//
+//  SplashView.swift
+//  Move
+//
+//  Created by Raul Pele on 10.08.2022.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack {
+            Color.primaryLight
+                .ignoresSafeArea()
+            Image("SplashScreenMoveLogo")
+            Image("SplashScreenScooter")
+                .offset(x: -116)
+            Image("SplashScreenMoveText")
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
