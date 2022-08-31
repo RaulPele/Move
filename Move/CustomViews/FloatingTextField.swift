54c8370565bd973a49b4de6a3fc08f1aa0582fe8
Raul Pele <raul.pele2001@gmail.com>
2022-08-31 16:19:23 +0300

    changed folder structure, grouping them by modules

--- Move/CustomViews/FloatingTextField.swift
+++ Move/CustomViews/FloatingTextField.swift
@@ -52,59 +52,6 @@ struct FloatingTextField: View {
     }
 }
 
-struct FloatingSecureField: View {
-    let title: String
-    @Binding var text: String
-    
-    @State var isSecured = true
-    @FocusState var isFocused: Bool
-    
-    var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                ZStack(alignment: .leading) {
-                    Text(title)
-                        .foregroundColor(.neutralGray)
-                        .font(text.isEmpty ? .body1() : .caption2())
-                        .offset(y: text.isEmpty ? 0 : -25)
-                        .scaleEffect(text.isEmpty ? 1 : 0.8, anchor: .leading)
-                        .padding(.vertical, text.isEmpty ? 10 : 0)
-                    
-                    Group {
-                        if isSecured {
-                            SecureField("", text: $text)
-                            
-                        } else {
-                            TextField("", text: $text)
-                            
-                        }
-                    }
-                    .foregroundColor(text.isEmpty ? .neutralGray : .neutralWhite)
-                    .disableAutocorrection(true)
-                    .font(.body1())
-                    .focused($isFocused)
-                    .textInputAutocapitalization(.never)
-                    
-                }
-                .animation(.spring(response: 0.2, dampingFraction: 0.5), value: text)
-                
-                if !text.isEmpty{
-                    Button {
-                        isSecured.toggle()
-                    } label: {
-                        Image(isSecured ? "eye" : "eye-off")
-                    }
-                }
-            }
-            
-            Divider()
-                .frame(height: isFocused ? 2 : 1)
-                .background(isFocused ? Color.neutralWhite : Color.neutralGray)
-                .padding(.top, 6)
-        }
-    }
-}
-
 struct FloatingTextField_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(devices) { device in
